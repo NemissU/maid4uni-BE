db583c4227385ca05b2a0b614fa6d0cccf1c410d
NemissU <namphse171651@fpt.edu.vn>
2023-10-27 13:27:59 +0700

    update heroku

--- src/main/java/com/swp391/maid4uni/App.java
+++ src/main/java/com/swp391/maid4uni/App.java
@@ -1,20 +1,8 @@
 package com.swp391.maid4uni;
 
-import com.swp391.maid4uni.entity.Account;
-import com.swp391.maid4uni.entity.Role;
-import com.swp391.maid4uni.repository.AccountRepository;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import java.time.LocalDate;
-import java.util.Date;
 
 /**
  * The type App.
