4a511b833ed261f5ae66ad96e70136b0f4b7ae41
NemissU <namphse171651@fpt.edu.vn>
2023-11-17 10:18:42 +0700

    Nam: Get all order by created at descending order

--- src/main/java/com/swp391/maid4uni/controller/OrderController.java
+++ src/main/java/com/swp391/maid4uni/controller/OrderController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,4 +51,13 @@ public class OrderController {
                 orderService.updateOrderStatus(request));
     }
 
+    @GetMapping(API_PARAMS.GET_ALL_ORDER)
+    public ResponseEntity<ResponseObject> getAllOrder(@PathVariable int page) {
+        log.info("Start get all order");
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("OK"
+                        , "GET ALL PACKAGE SUCCESSFULLY"
+                        , orderService.getAllOrder(page))
+        );
+    }
 }
--- src/main/java/com/swp391/maid4uni/repository/OrderRepository.java
+++ src/main/java/com/swp391/maid4uni/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.swp391.maid4uni.repository;
 
 import com.swp391.maid4uni.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +12,7 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findAllByCustomerIdAndLogicalDeleteStatus(int id, short logicalDeleteStatus);
 
     Order findByIdAndLogicalDeleteStatus(int id, int logicalDeleteStatus);
+
+    @Query(value = "SELECT o FROM Order o WHERE o.logicalDeleteStatus =:logicalDeleteStatus ORDER BY o.createdAt DESC")
+    List<Order> findAllOrderByCreatedAtDescWithOffSetAndLimit(@Param("logicalDeleteStatus")int logicalDeleteStatus, Pageable pageable);
 }
